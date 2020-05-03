f88268e1cc1ece40eb846285745bdb2ee50cef00
charanaks <43858050+charanaks@users.noreply.github.com>
2020-05-03 12:46:18 +0530

    Update SubsetProblem.py

--- SubsetProblem.py
+++ SubsetProblem.py
@@ -10,7 +10,7 @@ for i in range(1,len(l)+1):
         if l[i-1]>j:
             dp[i][j]=dp[i-1][j]
         else:
-            dp[i][j]=dp[i-1][j] or dp[i-1][j-l[i-1]] //important 
+            dp[i][j]=dp[i-1][j] or dp[i-1][j-l[i-1]] #important 
 if dp[len(l)][s]==0:
     print("False")
 else:
