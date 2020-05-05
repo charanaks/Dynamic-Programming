5c6e8c8ab45508cab3e76053ce852eeafac0c166
charanaks <43858050+charanaks@users.noreply.github.com>
2020-05-05 13:43:35 +0530

    Update MinimumMatrixCost.py

--- MinimumMatrixCost.py
+++ MinimumMatrixCost.py
@@ -17,20 +17,3 @@ for i in range(1,r):
     for j in range(1,c):
         dp[i][j]=m[i][j]+min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
 print(dp[-1][-1])
-k=m[-1][-1]
-path=[]
-path.append(k)
-r=r-1
-c=c-1
-while(r>0 and c>0):
-    mi=min(dp[r-1][c-1],dp[r-1][c],dp[r][c-1])
-    if mi==dp[r-1][c]:
-        r=r-1
-    elif mi==dp[r-1][c-1]:
-        r=r-1
-        c=c-1
-    else:
-        c=c-1
-    path.append(m[r][c])
-for _ in range(len(path)):
-    print(path.pop(),end=" ")
