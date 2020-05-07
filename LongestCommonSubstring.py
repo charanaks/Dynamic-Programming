4cc9d4ad67cba0f1b4329cf2fc805ed9155b4b43
charanaks <43858050+charanaks@users.noreply.github.com>
2020-05-07 12:29:54 +0530

    Update LongestCommonSubstring.py

--- LongestCommonSubstring.py
+++ LongestCommonSubstring.py
@@ -1,14 +1,18 @@
 s1=input()
 s2=input()
 result=0
+s=""
 dp=[[0 for k in range(len(s2)+1)]for l in range(len(s1)+1)]
-for i in range(len(s1)+1):
-    for j in range(len(s2)+1):
-        if i==0 or j==0:
-            dp[i][j]=0
-        elif s1[i-1]==s2[j-1]:
+for i in range(1,len(s1)+1):
+    for j in range(1,len(s2)+1):
+        if s1[i-1]==s2[j-1]:
             dp[i][j]=1+dp[i-1][j-1]
-        else:
-            dp[i][j]=0
-        result=max(result,dp[i][j])
+            if dp[i][j]>result:
+                result=dp[i][j]
+                ii=i-1
 print(result)
+while(result>0):
+    s=s+s1[ii]
+    ii=ii-1
+    result=result-1
+print(s[::-1])    
